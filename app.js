9329ac1fe0339aa76a3e18e9fad12fdcea7f4f11
Mayur Bhatia <bhatia.ma@northeastern.edu>
2021-02-10 03:21:40 -0500

    Write unit test

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const createDB = require("./src/loaders/create.db");
 
 const app = express();
+const PORT = process.env.PORT || 8081;
 
 var corsOptions = {
   origin: "http://localhost:8081",
 };
+
+if (process.env.NODE_ENV === 'test') {
+  console.log = function () {};
+}
 
 app.use(cors(corsOptions));
 
@@ -16,15 +22,26 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//load database
-require("./src/loaders/database");
+// Sync Database
+createDB.then(() => {
+  const db = require("./src/loaders/database");
+  db.sequelize
+    .sync({ force: true })
+    .then(() => {
+      console.log("Model synced");
+      // set port, listen for requests
+      app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}.`);
+        // set routes
+        require("./src/routes/auth.routes")(app);
+        require("./src/routes/user.routes")(app);
 
-// set route
-require('./src/routes/auth.routes')(app);
-require('./src/routes/user.routes')(app);
-
-// set port, listen for requests
-const PORT = process.env.PORT || 8081;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+        app.emit("serverStarted");
+      });
+    })
+    .catch((err) => {
+      err;
+    });
 });
+
+module.exports = app;
--- src/validators/verifyUser.js
+++ src/validators/verifyUser.js
@@ -1,3 +1,4 @@
+const db = require("../loaders/database");
 const User = db.user;
 
 const regex = /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/
