bda53697927d986388ef4d1a643a1894553fb605
Mayur Bhatia <bhatia.ma@northeastern.edu>
2021-02-11 02:35:31 -0500

    Add validations to check null values

--- src/validators/verifyUser.js
+++ src/validators/verifyUser.js
@@ -4,6 +4,11 @@ const User = db.user;
 const regex = /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,}$/;
 
 checkDuplicateEmail = (req, res, next) => {
+  if(!req.body.username){
+    return res.status(400).send({
+      message: "Cannot Create account! Username cannot be blank",
+    });
+  }
   User.findOne({
     where: {
       username: req.body.username,
@@ -11,7 +16,7 @@ checkDuplicateEmail = (req, res, next) => {
   }).then((user) => {
     if (user) {
       res.status(400).send({
-        message: "Cannot Create account! Email is already in use!",
+        message: "Cannot Create account! Username is already in use!",
       });
       return;
     }
@@ -20,9 +25,14 @@ checkDuplicateEmail = (req, res, next) => {
 };
 
 checkPassword = (req, res, next) => {
-  if (!regex.test(req.body.password)) {
+  if (!req.body.password) {
     return res.status(400).send({
-      message: "Password is too weak! Please make sure password has uppercase letters, lowercase letters, numbers, and special characters.",
+      message: "Password cannot be empty",
+    });
+  } else if (!regex.test(req.body.password)) {
+    return res.status(400).send({
+      message:
+        "Password is too weak! Please make sure password has uppercase letters, lowercase letters, numbers, and special characters.",
     });
   }
   next();
@@ -31,10 +41,10 @@ checkPassword = (req, res, next) => {
 checkEmailUpdate = (req, res, next) => {
   User.findOne({
     where: {
-      username: req.body.username,
+      username: req.user.username,
     },
-  }).then((user) => {
-    if (req.body.username != req.user.username) {
+  }).then(() => {
+    if ((req.body.username && (req.body.username != req.user.username)) || req.body.username === "") {
       res.status(400).send({
         message: "Username cannot be updated",
       });
@@ -44,10 +54,20 @@ checkEmailUpdate = (req, res, next) => {
   });
 };
 
+checkEmptyRequestBody = (req, res, next) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send({
+      message: "Request body cannot be empty"
+    });
+  }
+  next();
+};
+
 const verifyUser = {
   checkDuplicateEmail: checkDuplicateEmail,
   checkPassword: checkPassword,
   checkEmailUpdate: checkEmailUpdate,
+  checkEmptyRequestBody: checkEmptyRequestBody
 };
 
 module.exports = verifyUser;
