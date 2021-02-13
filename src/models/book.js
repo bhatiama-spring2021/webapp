46ebb165f9f8b7f20437569ddb60f15bb456e05a
Mayur Bhatia <bhatia.ma@northeastern.edu>
2021-02-13 14:58:40 -0500

    Create Book model and add APIs

---
+++ src/models/book.js
@@ -0,0 +1,57 @@
+module.exports = (sequelize, Sequelize) => {
+  const Book = sequelize.define(
+    "books",
+    {
+      book_id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV1,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      isbn: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      published_date: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      user_id: {
+        type: Sequelize.UUID,
+        references: {
+          model: "users",
+          key: "user_id",
+        },
+      },
+    },
+    {
+      createdAt: "book_created",
+    });
+
+Book.associate = function(models) {
+  Book.belongsTo(models.User, {
+    foreignKey: "user_id"
+  });
+};
+
+  return Book;
+};
