ccbd2d043216aeb4c0bfe8574dd040b137ca0783
Mayur Bhatia <bhatia.ma@northeastern.edu>
2021-03-10 22:48:24 -0500

    test workflow

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ const app = require("../app");
 const chai = require("chai");
 const request = require("supertest");
 const should = chai.should();
+const nock = require("nock");
 
 const testUser = {
   username: "test@gmail.com",
@@ -11,6 +12,10 @@ const testUser = {
 // Test auth /GET route
 describe("Test GET /v1/user/self", function () {
   it("should return status 200", function (done) {
+    nock("http://localhost:8080")
+      .get("/v1/user/self")
+      .reply(200, { message: "success" });
+
     request(app)
       .get("/v1/user/self")
       .auth(testUser.username, testUser.password, { type: "basic" })
