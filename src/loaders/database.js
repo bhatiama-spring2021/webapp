6c5567be8e552daad7003b0076804a4619137551
Mayur Bhatia <bhatia.ma@northeastern.edu>
2021-04-12 15:55:07 -0400

    set sequelize dialiect option to use ssl

--- src/loaders/database.js
+++ src/loaders/database.js
@@ -1,4 +1,7 @@
 const config = require("../config/db.config");
+const { QueryTypes } = require("sequelize");
+const log = require("../../logs");
+const logger = log.getLogger("logs");
 
 const db = {};
 
@@ -12,7 +15,10 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(configDB, config.USER, config.PASSWORD, {
   host: config.HOST,
   dialect: config.dialect,
-
+  dialectOptions: {
+    ssl: "Amazon RDS",
+    rejectUnauthorized: true,
+  },
   pool: {
     max: config.pool.max,
     min: config.pool.min,
@@ -20,6 +26,30 @@ const sequelize = new Sequelize(configDB, config.USER, config.PASSWORD, {
     idle: config.pool.idle,
   },
 });
+
+// check and log the validation result for RDS SSL connection type
+sequelize
+  .query(
+    "SELECT id, user, host, connection_type FROM performance_schema.threads pst INNER JOIN information_schema.processlist isp ON pst.processlist_id = isp.id",
+    {
+      type: QueryTypes.SELECT,
+    }
+  )
+  .then((query_res) => {
+    if (query_res == undefined || query_res == null || query_res.length == 0) {
+      logger.info(
+        "RDS DB SSL connection type result of query : SELECT id, user, host, connection_type FROM performance_schema.threads pst INNER JOIN information_schema.processlist isp ON pst.processlist_id = isp.id ; Result : SSL data not available"
+      );
+    } else {
+      logger.info(
+        " RDS DB SSL connection type result of query : SELECT id, user, host, connection_type FROM performance_schema.threads pst INNER JOIN information_schema.processlist isp ON pst.processlist_id = isp.id ; Result :"
+      );
+      logger.info(JSON.stringify(query_res));
+    }
+  })
+  .catch(err => {
+    logger.error("Error running sequelize Query: " + err);
+  });
 
 // Authenticate connection
 sequelize
